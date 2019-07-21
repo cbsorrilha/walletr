c34e1c68a1b0fccaba9fcc600e8a189652934a2c
Cesar de Barros <cbsorrilha@hotmail.com>
2019-07-21 17:08:26 -0300

    fix: fixed route disposition and active logic

--- src/components/molecules/navigation-link.js
+++ src/components/molecules/navigation-link.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import styled, { css } from 'styled-components';
-import FlexItem from '../atoms/flex-item';
-import Link from '../atoms/link';
+import React from "react";
+import styled, { css } from "styled-components";
+import Link from "../atoms/link";
 
 export default function NavigationLink({ active, label, ...props }) {
   return (
-    <NavigationLinkUI active={active}>
-      <Link {...props}>{label}</Link>
+    <NavigationLinkUI active={active} {...props}>
+      {label}
     </NavigationLinkUI>
   );
 }
 
-const NavigationLinkUI = styled(FlexItem)`
+const NavigationLinkUI = styled(Link)`
   height: 100%;
   display: flex;
   align-items: center;
@@ -20,6 +19,6 @@ const NavigationLinkUI = styled(FlexItem)`
       ? css`
           background-color: #e6e6e6;
         `
-      : ''};
+      : ""};
   padding: 0 1.5rem;
 `;
