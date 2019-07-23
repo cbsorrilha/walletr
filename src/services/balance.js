0a37cd4d9d889a0091f39ead1ff4c7e203166b7f
Cesar de Barros <cbsorrilha@hotmail.com>
2019-07-22 23:05:16 -0300

    WIP

--- src/services/balance.js
+++ src/services/balance.js
@@ -1,1 +1,14 @@
 // here goes the balance server
+import PouchDB from 'pouchdb';
+
+const db = new PouchDB('http://localhost:5984/kittens');
+
+export function getBalance() {
+  return db.get('balance');
+}
+
+export async function setBalance() {
+  const balance = await db.get('balance');
+  balance;
+  return db.put('balance');
+}
